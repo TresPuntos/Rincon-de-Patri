df813d11cd6f7895f347035ea60ebd7dfe70e8a0
TresPuntos <jordi@trespuntos.com>
2025-11-02 20:32:07 +0100

    Simplificar handler de Vercel y actualizar vercel.json

--- api/index.js
+++ api/index.js
@@ -1,8 +1,6 @@
 // Vercel Serverless Function Handler para Express
-// Este archivo es el punto de entrada para todas las rutas en Vercel
-
 const app = require('../index.js');
 
-// Exportar el handler para Vercel (debe ser una función o el app directamente)
+// Vercel necesita el handler exportado directamente
 module.exports = app;
 
